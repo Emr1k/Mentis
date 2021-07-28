06801f12edd988ea0e8feebb2ca4638f48cf79bf
emre <emrekovanci26@gmail.com>
2021-07-29 00:07:28 +0300

    Kamera Düzeltmesi

--- Assets/Scripts/camScript.cs
+++ Assets/Scripts/camScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class camScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Transform player;
+    public float turnSpeed;
+    private Vector3 diff;
+    public Vector3 offset;
+    public bool reverseX;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        diff = new Vector3(player.position.x, player.position.y + offset.x, player.position.z + offset.y);
+        if (reverseX) { turnSpeed *= -1; }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        /*
+        float mouseX;
+        float mouseY;
+        mouseX = Input.GetAxisRaw("Mouse X") * speed;
+        mouseY = Input.GetAxisRaw("Mouse Y") * speed;
+
+        transform.Rotate()
+    }
+        */
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
     }
 }
